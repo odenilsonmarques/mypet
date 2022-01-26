ccb36a69e4b46e178807fef11e8538d2939479a6
odenilson <odenilson.marques@gmz.solutions>
2022-01-26 08:18:13 -0300

    alteracao na pagia inicial

--- resources/lang/pt-BR/auth.php
+++ resources/lang/pt-BR/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Email e ou Senha Inválido.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente mais tarde',
 
 ];
--- resources/views/adminViews/welcome.blade.php
+++ resources/views/adminViews/welcome.blade.php
@@ -16,7 +16,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Acessar Carteira</a>
+                        <a  href="{{ route('login') }}" class="btn btn-primary">Acessar Carteira</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
@@ -42,7 +42,7 @@
                 <div class="col-lg-4 text-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-chat-square-dots-fill icon" viewBox="0 0 16 16" id="icon">
                         <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.5a1 1 0 0 0-.8.4l-1.9 2.533a1 1 0 0 1-1.6 0L5.3 12.4a1 1 0 0 0-.8-.4H2a2 2 0 0 1-2-2V2zm5 4a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-                    </svg><br>
+                    </svg>
                     <p class="text">
                         Olá me chamo fred, hoje tenho 6 anos, sou muito bricalhão e muito peralta au au! sou muito apegado aos meus donos e fico furioso quando 
                         eles não me dão atenção, faço qualquer coisa para chamar a atenção deles, até ja fiz pi pi. 
@@ -51,7 +51,7 @@
                 <div class="col-lg-4 text-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-palette-fill icon" viewBox="0 0 16 16" id="icon">
                     <path d="M12.433 10.07C14.133 10.585 16 11.15 16 8a8 8 0 1 0-8 8c1.996 0 1.826-1.504 1.649-3.08-.124-1.101-.252-2.237.351-2.92.465-.527 1.42-.237 2.433.07zM8 5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm4.5 3a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                </svg><br>
+                </svg>
                     <p class="text">
                         Sou da raça poodle. De acordo com a Federação Cinológica Internacional essa raça é oriunda da 
                         França, por ter sido reivindicada, porém ela já era há muito tempo criada em países da Europa 
@@ -61,7 +61,7 @@
                 <div class="col-lg-4 text-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16" id="icon">
                         <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-                    </svg><br>
+                    </svg>
                     <p class="text"> 
                         Moro na rua Dr José Silva em São José de Ribamar - MA, no bairro Moropoia, fica pertinho do parque da cidade, o numero da casa
                         é 65c. ha!! também tem o numero (98) 98106 -1009 caso queira entrar em contato. 
